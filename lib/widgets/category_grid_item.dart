ceb7f885fb0cfcccb22ea9907f519b7bbc736ebd
Mantas <mantasx5@gmail.com>
2023-10-15 21:00:53 +0200

    Added navigation to MealScreen while tapping category.

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({Key? key, required this.category}) : super(key: key);
+  const CategoryGridItem(
+      {Key? key, required this.category, required this.onSelectedCategory})
+      : super(key: key);
 
   final Category category;
+  final void Function() onSelectedCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onSelectedCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
       child: Container(
