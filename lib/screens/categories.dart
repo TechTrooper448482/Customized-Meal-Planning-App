4ed1e4c8a978eeb1e55654b226f6b64feae60ad8
Mantas <mantasx5@gmail.com>
2023-10-15 21:14:21 +0200

    Added meal filtering by passing data to the target screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) =>
-            const MealsScreen(title: "Some titele", meals: []),
+            MealsScreen(title: category.title, meals: filteredMeals),
       ),
     );
   }
@@ -34,7 +39,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectedCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             ),
         ],
