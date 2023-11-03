5052af7a1614b23aeefce6c72910d3ea734bcd18
Mantas <mantasx5@gmail.com>
2023-11-03 09:33:47 +0100

    Triggering a Notifier Method.

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
@@ -10,14 +12,35 @@ class MealDetailsScreen extends StatelessWidget {
   final Meal meal;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
             onPressed: () {
-              onToggleFavorite(meal);
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleFavoriteMeal(meal);
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    wasAdded
+                        ? '${meal.title} added to favorites.'
+                        : '${meal.title} removed from favorites.',
+                  ),
+                  duration: const Duration(seconds: 5),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      ref
+                          .read(favoriteMealsProvider.notifier)
+                          .toggleFavoriteMeal(meal);
+                    },
+                  ),
+                ),
+              );
             },
             icon: const Icon(Icons.star),
           ),
