a5419fb1f5ee346e10c9afc435a6529c782869cf
TechTrooper448482 <mowaiskhan658@gmail.com>
2025-04-16 20:37:12 +0500

    Meals App

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/tabs.dart';
 
-/// The theme data used in the app.
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: Colors.black, //const Color.fromARGB(255, 131, 57, 0),
+    seedColor: Colors.black,
   ),
-  // textTheme: GoogleFonts.latoTextTheme(),
 );
 
 void main() {
